c8d9247d74c5788d9d9f025e2e727e68407bd7f9
gabean13 <choegabin171@gmail.com>
2025-02-27 12:09:03 +0900

    feat : 자기소개 위키 구조 구현

--- js/index.js
+++ js/index.js
@@ -17,3 +17,31 @@ console.log(
 );
 
 console.log("알맞은 스크립트를 작성하세요");
+
+function addComment() {
+    const commentText = document.getElementById('comment-input').value;
+    console.log(commentText);
+    if (commentText.trim() !== '') {
+        const commentCard = document.createElement('div');
+        commentCard.className = 'comment-card';
+
+        commentCard.innerHTML = `
+            <div class="comment-header">
+                <img src="./images/comment-author-icon.png" alt="avatar" class="avatar">
+                <div class="author">방문자</div>
+            </div>
+            <div class="comment-content">
+                ${commentText}
+            </div>
+        `;
+
+        document.getElementById('comment-section').appendChild(commentCard);
+        document.getElementById('comment-input').value = '';
+
+        alert("댓글이 등록되었습니다");
+    }
+}
+
+function cancelComment() {
+    document.getElementById('comment-input').value = '';
+}
